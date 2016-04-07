acb229625ba3edb336e4a11c2ad012d56a4de040
TheGoodFella <galas.daniele@gmail.com>
2016-04-07 21:33:22 +0200

    add files

---
+++ cprime.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+typedef enum {true, false} bool;
+
+int main(int argc, char *argv[]) {
+	int count;
+	int num=100;
+	
+	if(argc>1)
+	{
+		num=0;
+		int len = strlen(argv[1]);
+	
+		//convert number passed in argument from string to integer
+		int i;
+		for(i=0;i<len;i++)
+		{
+			num=num*10+(argv[1][i]-'0');
+		}
+	}	
+	printf("\nstart checking...");
+	count=Count(num);
+	printf("\nprimes until %d : %d\n",num, count);
+	
+	return 0;
+}
+bool IsPrime(int num)
+{
+		if(num<=3)
+			return true;
+		
+		int i;
+		for(i=2;i*i<=num;i++)
+			if(num%i==0)
+				return false;
+		return true;
+}
+int Count(int max)
+{
+	int c=0;
+	
+	int i;
+	for(i=2;i<=max;i++)
+		if(IsPrime(i)==true)
+			c=c+1;
+	return c;
+}
+
+
